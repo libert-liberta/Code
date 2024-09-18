01db8e74adf563220bb0c8c8d6758600e05baba6
libert-liberta <liberaldenial@gmail.com>
2024-09-18 22:42:17 +0900

    add

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code
+VSCodeの設定類
--- User/.vscode_extensions/export_install_list_extensions.sh
+++ User/.vscode_extensions/export_install_list_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Windowsで実行する場合はGit Bashに関連付けをするか、Git Bashで直接実行してください
-readonly FILE_NAME="export_extensions.source"
+readonly FILE_NAME="install_extensions.sh"
 echo "#!/bin/bash" > $FILE_NAME
 echo "# Windowsで実行する場合はGit Bashに関連付けをするか、Git Bashで直接実行してください" >> $FILE_NAME
-code --list-extensions | xargs -I extension_name echo "extensions+=("extension_name")" >> $FILE_NAME
+code --list-extensions | xargs -L 1 echo code --install-extension >> $FILE_NAME
